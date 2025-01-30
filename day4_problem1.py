df60d1ebae78c6bb95f8672dee919dad88828482
Nadia Santillán <nadiasantillan@gmail.com>
2025-01-30 07:45:09 -0300

    Comments

--- day4_problem1.py
+++ day4_problem1.py
@@ -1,7 +1,10 @@
+# A small Elf who lives on the station tugs on your shirt; she'd like to know if you could help her with her word 
+# search. She only has to find one word: XMAS.
+# This word search allows words to be horizontal, vertical, diagonal, written backwards, or even overlapping other words.
 import re
 import time
 
-def find_horizontal(lines):
+def find_horizontal(lines): 
     s = 0
     for line in lines:
         s += len(re.findall("XMAS", line)) + len(re.findall("XMAS", line[::-1]))
@@ -11,10 +14,23 @@ def find_vertical(lines):
     return find_horizontal(["".join(lines[j][i] for j in range(len(lines))) for i in range(len(lines))])
 
 def diagonals(lines):
+    #   0 1 2 3 4 5 6 7 8 9   #   0 1 2 3 4 5 6 7 8 9   #   0 1 2 3 4 5 6 7 8 9   #   0 1 2 3 4 5 6 7 8 9  
+    # 0 x                     # 0   x                   # 0                       # 0       
+    # 1   x                   # 1     x                 # 1 m                     # 1                   m                   
+    # 2     x                 # 2       x               # 2   a                   # 2                 a             
+    # 3       x               # 3         x             # 3     c                 # 3               c               
+    # 4         x             # 4           x           # 4       a               # 4             a             
+    # 5           x           # 5             x         # 5         d             # 5           d        
+    # 6             x         # 6               x       # 6           a           # 6         a         
+    # 7               x       # 7                 x     # 7             m         # 7       m
+    # 8                 x     # 8                   x   # 8               i       # 8     i
+    # 9                   x   # 9                       # 9                 a     # 9   a   
+    # r 0 1 2 3 4 5 6 7 8 9   # r   0 1 2 3 4 5 6 7 8   # r 1 2 3 4 5 6 7 8 9     # r   9 8 7 6 5 4 3 2 1
+    # c 0 1 2 3 4 5 6 7 8 9   # c   1 2 3 4 5 6 7 8 9   # c 0 1 2 3 4 5 6 7 8     # c   1 2 3 4 5 6 7 8 9
     return (["".join(lines[j][i+j] for j in range(len(lines)-i)) for i in range(len(lines))] +
         ["".join(lines[i+j][j] for j in range(len(lines)-i)) for i in range(1, len(lines))])
 
-def find_diagonals(lines):
+def find_diagonals(lines): 
     rlines =[line[::-1] for line in lines]
     return find_horizontal(diagonals(lines)+diagonals(rlines))
 
--- day4_problem2.py
+++ day4_problem2.py
@@ -1,3 +1,6 @@
+# The Elf looks quizzically at you. Did you misunderstand the assignment?
+# Looking for the instructions, you flip over the word search to find that this isn't actually an XMAS puzzle; 
+# it's an X-MAS puzzle in which you're supposed to find two MAS in the shape of an X. 
 import time
 
 ROTATIONS = ["MMSS", "SSMM", "MSSM", "SMMS"]
@@ -7,7 +10,10 @@ lines = [line.strip() for line in open("data/day4.txt")]
 start = time.time()
 print(sum(1 
           for i in range(1, len(lines)-1) 
-            for j in range(1, len(lines)-1) 
+            for j in range(1, len(lines)-1)
+                # M   M   # S   M   # S   S   # M   S
+                #   A     #   A     #   A     #   A
+                # S   S   # s   M   # M   M   # M   S
                 if lines[i][j] == 'A' and 
                 lines[i-1][j-1]+lines[i-1][j+1]+lines[i+1][j+1]+lines[i+1][j-1] in ROTATIONS))
 
